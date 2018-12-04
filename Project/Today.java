bf4b21218c5bda36214743fa20f04c6ae3bb03ef
Kalea Bartolotto <kalea.bar@lvjusd.k12.ca.us>
2018-12-04 12:41:30 -0800

    I just ate lunch

---
+++ Project/Today.java
@@ -0,0 +1,18 @@
+
+
+public class Today
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+   
+    public Today()
+    {
+        // initialise instance variables
+        System.out.println("HI");
+        
+        
+    }
+
+
+}
